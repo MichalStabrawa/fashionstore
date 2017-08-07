1db5eb641699f2c05ad4a68dce85733325ec29b6
Michal Stabrawa <mistaaqua@gmail.com>
2017-08-07 23:52:37 +0200

    Nowe

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function () {
+    var okno = $(window);
+    var menu = $('#main-nav');
+
+   if (okno.scrollTop() >= 100) {
+      menu.addClass('scroll');
+   }
+
+   okno.scroll(function () {
+
+      if (okno.scrollTop() >= 100) {
+            menu.addClass('scroll');
+       } else {
+            menu.removeClass('scroll');
+        }
+  });
+
+    /*Smoth scroll*/
+
+    $(document).on('click', 'a[href^="#"]', function(event) {
+       event.preventDefault();
+var menuHeight = $('#main-nav').height();
+       $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
+        }, 500);
+  });
+
+
+});
+
+/*Toogle-------------------------------------------------------------------------------------------------------------- */
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $("#div1").toggle(1000);
+    });
+});
+
+
+function wyslij(){
+    var pokazDiva=document.getElementById('pokaz')  
+    document.getElementById('pokaz').style="display:block"
+}
+
+document.getElementById('button1').addEventListener('click',wyslij)
